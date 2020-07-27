b52033d9bc49160e9b91bc4d5328ab13d797dcce
Steve Condylios <steve.condylios@gmail.com>
2020-07-28 08:12:55 +1000

    Documentation completed for exchange rate capabilities

--- R/currencies.R
+++ R/currencies.R
@@ -1,7 +1,21 @@
 
-## NOTE: THIS FILE IS IN .Rbuildignore (and hence must be removed before it takes effect)
 
 
+
+
+#' Display link to further information
+#'
+#'
+#' @name display_api_info
+#'
+#' @usage display_api_info()
+#'
+#'
+#' @examples
+#' \dontrun{
+#' # Display a message indicating where further documentation can be found
+#' }
+#'
 
 
 
@@ -25,6 +39,33 @@ display_api_info <- function() {
 
 
 
+#' Retrieve available currencies and their respective symbols/codes
+#' @name currencies
+#'
+#' @usage currencies()
+#'
+#' @return A data.frame of available currencies and their respective symbols/codes
+#'
+#' @import dplyr purrr
+#' @importFrom jsonlite fromJSON
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Display available currencies and their respective symbols/codes
+#' currencies()
+#' #                                description code
+#' # 1              United Arab Emirates Dirham  AED
+#' # 2                           Afghan Afghani  AFN
+#' # 3                             Albanian Lek  ALL
+#' # 4                            Armenian Dram  AMD
+#' # 5            Netherlands Antillean Guilder  ANG
+#' # 6                           Angolan Kwanza  AOA
+#' # 7                           Argentine Peso  ARS
+#' }
+#'
+#'
 
 
 currencies <- function() {
@@ -56,9 +97,53 @@ currencies <- function() {
 
 
 
+#'
+#' Retrieve the latest exchange rates between the provided currency code
+#' @name exchange_rate_latest
+#'
+#' @usage exchange_rate_latest(currency)
+#'
+#' @param currency A currency code (see currencies() for supported codes)
+#'
+#' @return A data.frame containing the latest exchange rates between the provided currency code
+#'     and each other available currency
+#'
+#' @import dplyr purrr
+#' @importFrom jsonlite fromJSON
+#' @importFrom utils stack
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' exchange_rate_latest("AUD")
+#' # Daily AUD exchange rate as at end of day 2020-07-27 GMT
+#' #     currency one_aud_is_equivalent_to
+#' # 1        AED                  2.61894
+#' # 2        AFN                 54.47724
+#' # 3        ALL                 75.51799
+#' # 4        AMD                343.40193
+#' # 5        ANG                  1.26829
+#' # 6        AOA                400.54604
+#'
+#'
+#' # Defaults to USD
+#' exchange_rate_latest()
+#' # Daily USD exchange rate as at end of day 2020-07-27 GMT
+#' #     currency one_usd_is_equivalent_to
+#' # 1        AED                   3.6730
+#' # 2        AFN                  76.4035
+#' # 3        ALL                 105.9129
+#' # 4        AMD                 481.6162
+#' # 5        ANG                   1.7788
+#' # 6        AOA                 561.7599
+#'
+#'
+#' }
+#'
 
-
-latest_exchange_rate <- function(currency = "USD") {
+exchange_rate_latest <- function(currency = "USD") {
 
   display_api_info()
 
@@ -69,8 +154,8 @@ latest_exchange_rate <- function(currency = "USD") {
   # "EOD / End of Day historical exchange rates, which become available at
   # 00:05am GMT for the previous day and are time stamped at one second before midnight."
 
-  options(scipen=999)
-  options(digits=1)
+  # options(scipen=999)
+  # options(digits=1)
 
   col_name <- paste0("one_", tolower(currency), "_is_equivalent_to")
 
@@ -82,8 +167,6 @@ latest_exchange_rate <- function(currency = "USD") {
 
 }
 
-latest_exchange_rate()
-latest_exchange_rate("AUD")
 
 
 
@@ -98,7 +181,46 @@ latest_exchange_rate("AUD")
 
 
 
-# This simply creates date ranges so as to beach up large API calls into many smaller ones
+
+
+#'
+#' Creates date ranges so as to batch up large API calls into many smaller ones
+#' @name make_dates
+#'
+#' @usage make_dates(start_date, end_date, n_days)
+#'
+#' @param start_date A start date (of the form "2010-01-01")
+#' @param end_date An end date
+#' @param n_days The maximum number of days in each period
+#'
+#' @return A data.frame containing start and end dates for periods of length no longer than n_days
+#'
+#' @import dplyr
+#'
+#'
+#' @examples
+#'
+#' # Simple test
+#' start_date = "2010-01-01"
+#' end_date = "2020-06-30"
+#' n_days = 365
+#' priceR:::make_dates(start_date, end_date, n_days)
+#'
+#' # With lots of periods
+#' start_date = "2010-01-01"
+#' end_date = "2020-06-30"
+#' n_days = 20
+#' priceR:::make_dates(start_date, end_date, n_days)
+#'
+#' # Less than one period
+#' start_date = "2020-01-01"
+#' end_date = "2020-06-30"
+#' n_days = 365
+#' priceR:::make_dates(start_date, end_date, n_days)
+#'
+#'
+#'
+
 
 make_dates <- function(start_date, end_date, n_days) {
 
@@ -138,23 +260,6 @@ make_dates <- function(start_date, end_date, n_days) {
 }
 
 
-# Simple test
-start_date = "2010-01-01"
-end_date = "2020-06-30"
-n_days = 365
-make_dates(start_date, end_date, n_days)
-
-# With lots of periods
-start_date = "2010-01-01"
-end_date = "2020-06-30"
-n_days = 20
-make_dates(start_date, end_date, n_days)
-
-# Less than one period
-start_date = "2020-01-01"
-end_date = "2020-06-30"
-n_days = 365
-make_dates(start_date, end_date, n_days)
 
 
 
@@ -170,9 +275,39 @@ make_dates(start_date, end_date, n_days)
 
 
 
-# Helper for next function
 
-retrieve_historical_rates <- function(start_date, end_date, from, to) {
+#' Retrieve historical exchange rates
+#'
+#' Retrieves historical exchange rates between a currency pair - retrieves max. 365 days' data
+#' @name retrieve_historical_rates
+#'
+#' @usage retrieve_historical_rates(from, to, start_date, end_date)
+#'
+#' @param from A currency code (see currencies() for supported codes)
+#' @param to A currency code
+#' @param start_date A start date (of the form "2010-01-01")
+#' @param end_date An end date
+#'
+#'
+#' @return A data.frame containing exchange rate data for select currency pair
+#'
+#' @import dplyr purrr
+#' @importFrom jsonlite fromJSON
+#' @importFrom utils stack
+#'
+#' @examples
+#' \dontrun{
+#' # Note date range >365 days', yet only 365 days' returned.
+#' # Use historical_exchange_rates() for > 365 days'.
+#'  priceR:::retrieve_historical_rates("USD", to = "AUD",
+#'                                     start_date = "2018-01-01",
+#'                                     end_date = "2020-06-30")
+#' }
+#'
+#'
+
+
+retrieve_historical_rates <- function(from, to, start_date, end_date) {
 
   # "https://api.exchangerate.host/timeseries?start_date=2020-01-01&end_date=2020-01-04"
   endpoint <- paste0("https://api.exchangerate.host/timeseries?start_date=",
@@ -210,6 +345,47 @@ retrieve_historical_rates <- function(start_date, end_date, from, to) {
 
 
 
+#' Retrieve historical exchange rates
+#'
+#' Retrieves historical exchange rates between a currency pair
+#' @name historical_exchange_rates
+#'
+#' @usage historical_exchange_rates(from, to, start_date, end_date)
+#'
+#' @param from A currency code (see currencies() for supported codes)
+#' @param to A currency code
+#' @param start_date A start date (of the form "2010-01-01")
+#' @param end_date An end date
+#'
+#'
+#' @return A data.frame containing exchange rate data for select currency pair
+#'
+#' @import dplyr purrr
+#' @importFrom jsonlite fromJSON
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Note date range >365 days', and returns 912 rows (as expected)
+#' historical_exchange_rates("USD", to = "AUD", start_date = "2018-01-01", end_date = "2020-06-30")
+#'
+#' historical_exchange_rates("USD", to = "AUD",
+#'                           start_date = "2020-01-01", end_date = "2020-06-30")
+#'
+#'
+#' historical_exchange_rates("AUD", to = "USD",
+#'                           start_date = "2010-01-01", end_date = "2020-06-30")
+#'
+#'
+#' historical_exchange_rates("AUD", to = "USD",
+#'                           start_date = "2010-01-01", end_date = "2020-06-30")
+#'
+#'
+#' }
+#'
+#'
+
 
 historical_exchange_rates <- function(from, to, start_date, end_date) {
 
@@ -225,16 +401,6 @@ historical_exchange_rates <- function(from, to, start_date, end_date) {
 
 
 
-historical_exchange_rates("USD", to = "AUD",
-                          start_date = "2020-01-01", end_date = "2020-06-30")
-
-
-historical_exchange_rates("AUD", to = "USD",
-                          start_date = "2010-01-01", end_date = "2020-06-30")
-
-
-historical_exchange_rates("AUD", to = "USD",
-                          start_date = "2010-01-01", end_date = "2020-06-30")
 
 
 
