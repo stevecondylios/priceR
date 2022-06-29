ccfdf2d14239256c95f5057806b00b6861604ac2
Steve Condylios <steve.condylios@gmail.com>
2022-06-29 20:39:23 +1000

    Remove lubridate and curl from adjust_for_inflation test helper (they're not necessary for tests)

--- tests/testthat/helper-adjust_for_inflation.R
+++ tests/testthat/helper-adjust_for_inflation.R
@@ -1,9 +1,7 @@
 library(jsonlite)
 library(dplyr)
-library(lubridate)
 library(purrr)
 library(testthat)
-library(curl)
 # options(digits = 22)
 
 # Note that this .R script doesn't know about functions in priceR
