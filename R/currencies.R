1be857ae96e5dec9b4d279ed3223f8822470fee8
Steve Condylios <steve.condylios@gmail.com>
2022-08-21 13:00:27 +1000

    Hard code currencies for validation, rather than retrieving them via API to reduce API calls

--- R/currencies.R
+++ R/currencies.R
@@ -411,18 +411,40 @@ retrieve_historical_rates <- function(from, to, start_date, end_date) {
 
 historical_exchange_rates <- function(from, to, start_date, end_date) {
 
-    # Validate currencies
+  # Validate currencies
 
-  valid_currencies <- priceR::currencies()
+  # Retrieved via: priceR::currencies()
+  # Hard coded to avoid excessive API calls
+  valid_currency_codes <- c(
+    "AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG",
+    "AZN", "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND",
+    "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BZD", "CAD",
+    "CDF", "CHF", "CLF", "CLP", "CNH", "CNY", "COP", "CRC", "CUC",
+    "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP", "ERN",
+    "ETB", "EUR", "FJD", "FKP", "GBP", "GEL", "GGP", "GHS", "GIP",
+    "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF",
+    "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD",
+    "JOD", "JPY", "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD",
+    "KYD", "KZT", "LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD",
+    "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MRU", "MUR",
+    "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK",
+    "NPR", "NZD", "OMR", "PAB", "PEN", "PGK", "PHP", "PKR", "PLN",
+    "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SBD", "SCR",
+    "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "SSP", "STD",
+    "STN", "SVC", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP",
+    "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS",
+    "VEF", "VES", "VND", "VUV", "WST", "XAF", "XAG", "XAU", "XCD",
+    "XDR", "XOF", "XPD", "XPF", "XPT", "YER", "ZAR", "ZMW", "ZWL"
+    )
 
   from = toupper(from)
   to = toupper(to)
 
-  if (!all(c(from, to) %in% valid_currencies$code)){
-    invalid_currencies <- c(from, to)[which(!c(from, to) %in% valid_currencies$code)]
+  if (!all(c(from, to) %in% valid_currency_codes)){
+    invalid_currencies <- c(from, to)[which(!c(from, to) %in% valid_currency_codes)]
     error_message = paste0("Invalid currency code(s): " ,"\"", paste0(invalid_currencies, collapse="\", \""), "\"",
 ". Run currencies()
-  to view all ", nrow(valid_currencies), " valid currency codes.")
+  to view all ", length(valid_currency_codes), " valid currency codes.")
     stop(error_message)
   }
 
